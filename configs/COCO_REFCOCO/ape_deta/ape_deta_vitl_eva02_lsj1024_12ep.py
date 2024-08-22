af6fec94c91b88a39e0bbd18b0340efaf2afb1af
user name <bhathiya@eidf144-main.vms.os.eidf.epcc.ed.ac.uk>
2024-08-22 10:22:29 +0000

    configs/COCO_REFCOCO/ape_deta/ape_deta_vitl_eva02_lsj1024_12ep.py

--- configs/COCO_REFCOCO/ape_deta/ape_deta_vitl_eva02_lsj1024_12ep.py
+++ configs/COCO_REFCOCO/ape_deta/ape_deta_vitl_eva02_lsj1024_12ep.py
@@ -39,6 +39,7 @@ model.model_vision.backbone = L(SimpleFeaturePyramid)(
         out_feature="last_feat",
         use_act_checkpoint=True,
         xattn=True,
+        swiglu=True,
     ),
     in_feature="${.net.out_feature}",
     out_channels=256,
